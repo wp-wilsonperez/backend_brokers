a08390727aee733f27d7455856c70a5a10bf3c17
Dante Cuevas <danieldantecuevas@gmail.com>
2017-04-27 03:42:51 -0500

    upload image fron user and correcion of field lastName and phone

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,9 +1,34 @@
 
 import sha1 from 'sha1';
 import moment from 'moment';
+import multer from 'multer';
+import fs from 'fs';
 import acl from "../configs/acl";
 
 import User from "../models/user";
+
+const pathRender = `uploads/user`;
+const pathUser = `./public/${pathRender}`;
+
+if (!fs.existsSync(pathUser)){
+    fs.mkdirSync('user');
+}
+
+let storage = multer.diskStorage({
+   destination: function(req, file, callback) {
+      callback(null, pathUser);
+   },
+   filename: function(req, file, callback){
+      var basename = file.originalname.split(/[\\/]/).pop(),
+      pos = basename.lastIndexOf(".");
+      if (basename === "" || pos < 1)
+         return "";
+      callback(null, file.fieldname + '-' + Date.now() + '.' + basename.slice(pos + 1));
+   }
+});
+
+let upload = multer({storage: storage}).single("userImg");
+
 
 let userController = function (app, control={auth, passport, acl}){
 
@@ -104,11 +129,13 @@ let userController = function (app, control={auth, passport, acl}){
 
       let update = {
          name: req.body.name,
-         lastname: req.body.lastname,
+         lastName: req.body.lastName,
          cedula: req.body.cedula,
          password: sha1(req.body.password),
+         phone: req.body.phone,
          dateBirthday: req.body.dateBirthday,
          idRol: req.body.idRol,
+         userImg: req.body.userImg,
          dateUpdate: moment(),
          userUpdate: req.user._id,
          Enabled: req.body.Enabled
@@ -130,11 +157,13 @@ let userController = function (app, control={auth, passport, acl}){
 
       let user = new User({
          name: req.body.name,
-         lastname: req.body.lastname,
+         lastName: req.body.lastName,
          cedula: req.body.cedula,
          password: sha1(req.body.password),
+         phone: req.body.phone,
          dateBirthday: req.body.dateBirthday,
          idRol: req.body.idRol,
+         userImg: req.body.userImg,
          dateCreate: moment(),
          userCreate: req.user._id,
          dateUpdate: moment(),
@@ -168,6 +197,32 @@ let userController = function (app, control={auth, passport, acl}){
          } else {
             res.send({msg: 'ERR', err: err});
          }            
+      });
+
+   });
+
+   app.post('/userImg', [control.auth, controller, control.acl], (req, res) => {
+
+      upload(req , res , function(err) {
+         if(!err){
+            let $userImg = `${req.file.filename}`;
+            res.send({msg: "OK", userImg: $userImg, path: pathRender});
+         } else {
+            res.send({msg: 'ERR', err: err});
+         }
+      });
+
+   });
+
+   app.post('/userImg/:name', [control.auth, controller, control.acl], (req, res) => {
+
+      let $userImgPath = `${pathUser}/${req.params.name}`;
+      fs.unlink($userImgPath, function (err) {
+         if(!err){
+            res.send({msg: "OK"});
+         } else {
+            res.send({msg: 'ERR', err: err});
+         }
       });
 
    });
--- app/models/user/index.js
+++ app/models/user/index.js
@@ -3,17 +3,18 @@ import mongoose from 'mongoose';
 
 let UserSchema = new mongoose.Schema({
 	name: {type: String, require: true},
-	lastname: {type: String, require: true},
+	lastName: {type: String, require: true},
 	cedula: {type: String, require: true},
 	password: {type: String, require: true},
 	phone: {type: String, require: true},
 	dateBirthday: {type: String, require: true},
 	idRol: {type: Number, require: true},
+	userImg: {type: Number, default: ""},
+	Enabled: {type: Number, require: true},
 	dateCreate: {type: Date, require: true},
 	userCreate: {type: Number, require: true},
 	dateUpdate: {type: String, require: true},
-	userUpdate: {type: Number, require: true},
-	enabled: {type: Number, require: true}
+	userUpdate: {type: Number, require: true}
 });
 
 export default mongoose.model('User', UserSchema)
